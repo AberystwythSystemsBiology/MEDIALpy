bd640190f8d89c40c8c5ba39375e598f89522e07
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-08 15:16:24 +0000

    Back to basics

--- setup.py
+++ setup.py
@@ -1,95 +1,23 @@
 
-import io
-import os
-import sys
-from shutil import rmtree
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-from setuptools import setup, Command
+from setuptools import setup, find_packages
 
-NAME = 'medialpy'
-PACKAGE = 'medialpy'
-DESCRIPTION = ('MEDIcal Abbreviations Lookup in Python')
-URL = 'https://github.com/AberystwythSystemsBiology/medialpy'
-EMAIL = 'keiron.oshea@wales.nhs.uk'
-AUTHOR = 'Keiron O\'Shea'
-REQUIRES_PYTHON = '>=3.6.0'
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-about = {}
-with open(os.path.join(here, PACKAGE, '__version__.py')) as f:
-    exec(f.read(), about)
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
+    name='medialpy',
+    version='0.0.2',
+    description='MEDIcal Abbreviations Lookup in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
+    url='https://github.com/AberystwythSystemsBiology/medialpy',
+    author='KeironO',
+    author_email='keiron.oshea@wales.nhs.uk',
+    packages=find_packages(),
     include_package_data=True,
-    packages=[PACKAGE],
-    zip_safe=False,
-    license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-    ],
-    cmdclass={
-        'upload': UploadCommand,
-    },
 )
