255c94477dda0462478183836f109c456febf0a9
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-08 15:18:51 +0000

    Delete __version__.py

--- medialpy/__version__.py
+++
@@ -1,8 +0,0 @@
-__title__ = 'medialpy'
-__description__ = ('MEDIcal Abbreviations Lookup in Python')
-__url__ = 'https://github.com/AberystwythSystemsBiology/medialpy'
-__version__ = '0.0.1'
-__author__ = 'Keiron OShea'
-__author_email__ = 'keiron.oshea@wales.nhs.uk'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2022 Keiron O\'Shea'
