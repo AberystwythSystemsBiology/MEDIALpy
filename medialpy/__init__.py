952a9c35c72aae212e280ee96f6ab221201a7eb6
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-08 14:17:44 +0000

    This all smells great.

---
+++ medialpy/__init__.py
@@ -0,0 +1,38 @@
+import gzip
+import json
+import os
+
+from typing import Optional
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+with gzip.open(os.path.join(here, "abbrv.json.gz")) as infile:
+    annotations = json.load(infile)
+
+class MedicalAbbreviation:
+    def __init__(self, abbreviation: str, meaning: str):
+        self.abbreviation = abbreviation
+        self.meaning = meaning
+
+    def __str__(self):
+        return self.abbreviation
+    
+    def __repr__(self):
+        return "<MedicalAbbreviation: %s>" % (self)
+
+    def __hash__(self):
+        return hash(self.abbreviation)
+    
+
+def exists(s: str) -> bool:
+    if not s:
+        return False
+    return bool(annotations.get(s, False))
+
+def find(s: str) -> Optional[MedicalAbbreviation]:
+    if not s:
+        return
+    meaning = annotations.get(s)
+    if meaning is None:
+        return
+    return MedicalAbbreviation(s, meaning)
