161b4c2ea0cbc2375c72244b479f84d83a2ed191
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-08 15:11:43 +0000

    Revert "Cleaned up :D"
    This reverts commit 2d492736fd2d08bfdf0ff7711bc615a07c6c8c78.

--- setup.py
+++ setup.py
@@ -1,3 +1,95 @@
-from setuptools import setup
 
-setup()
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+NAME = 'medialpy'
+PACKAGE = 'medialpy'
+DESCRIPTION = ('MEDIcal Abbreviations Lookup in Python')
+URL = 'https://github.com/AberystwythSystemsBiology/medialpy'
+EMAIL = 'keiron.oshea@wales.nhs.uk'
+AUTHOR = 'Keiron O\'Shea'
+REQUIRES_PYTHON = '>=3.6.0'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+with open(os.path.join(here, PACKAGE, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    include_package_data=True,
+    packages=[PACKAGE],
+    zip_safe=False,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
