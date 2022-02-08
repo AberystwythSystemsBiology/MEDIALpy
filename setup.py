ee5bf491846e79eb2fcd94f0992507f909e5b63d
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-08 16:26:20 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 setup(
     name='medialpy',
-    version='0.0.2',
+    version='0.0.3',
     description='MEDIcal Abbreviations Lookup in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
